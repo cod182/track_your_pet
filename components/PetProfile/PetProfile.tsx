a373149fe9a9b306ea0b2cf9e6db7befe3cca06c
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-09-12 18:32:50 +0100

    refactorign petProfil comp for multi use

--- components/PetProfile/PetProfile.tsx
+++ components/PetProfile/PetProfile.tsx
@@ -13,11 +13,11 @@ declare interface petProfileProps {
   owner: Boolean;
 }
 
-const PetProfile = (petData: petProfileProps) => {
+const PetProfile = ({ petData, owner }: petProfileProps) => {
   const websiteUrl = process.env.NEXT_PUBLIC_WEBSITE_URL;
   const router = useRouter();
 
-  const pet = petData.petData;
+  const pet = petData;
   const {
     _id,
     petName,
@@ -80,44 +80,46 @@ const PetProfile = (petData: petProfileProps) => {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center my-8">
-      <section className="w-full h-fit flex flex-col sm:flex-row justify-evenly items-center">
-        <a
-          href={`/my-account/pets/edit-pet?id=${pet._id}`}
-          className="text-center my-2 w-fit min-w-[100px] h-fit py-2 px-4 bg-gradient-to-tr to-primary from-cyan-400 hover:to-cyan-600 rounded-lg shadow-xl font-semibold text-white hover:text-black hover:shadow-inner transition-all ease-in duration-400"
-        >
-          Edit
-        </a>
-        <div className="my-2 h-fit w-fit flex flex-col justify-center items-center">
-          <button
-            id="delete-button"
-            onClick={() => deleteClick('deleteCheck')}
-            className="w-fit min-w-[100px] h-fit py-2 px-4 bg-gradient-to-tr to-red-200 from-red-400 hover:to-red-600 rounded-lg shadow-xl font-semibold text-white hover:text-black hover:shadow-inner transition-all ease-in duration-300"
+      {owner && (
+        <section className="w-full h-fit flex flex-col sm:flex-row justify-evenly items-center">
+          <a
+            href={`/my-account/pets/edit-pet?id=${pet._id}`}
+            className="text-center my-2 w-fit min-w-[100px] h-fit py-2 px-4 bg-gradient-to-tr to-primary from-cyan-400 hover:to-cyan-600 rounded-lg shadow-xl font-semibold text-white hover:text-black hover:shadow-inner transition-all ease-in duration-400"
           >
-            <span id="delete-button-text">Delete</span>
-          </button>
-          {/* Selection button container Delete / Cancel */}
-          <div
-            id="selection-container"
-            className="bg-gray-300 w-fit h-fit overflow-hidden flex flex-row justify-between items-center transition-all duration-1000 ease-in rounded-b-md
+            Edit
+          </a>
+          <div className="my-2 h-fit w-fit flex flex-col justify-center items-center">
+            <button
+              id="delete-button"
+              onClick={() => deleteClick('deleteCheck')}
+              className="w-fit min-w-[100px] h-fit py-2 px-4 bg-gradient-to-tr to-red-200 from-red-400 hover:to-red-600 rounded-lg shadow-xl font-semibold text-white hover:text-black hover:shadow-inner transition-all ease-in duration-300"
+            >
+              <span id="delete-button-text">Delete</span>
+            </button>
+            {/* Selection button container Delete / Cancel */}
+            <div
+              id="selection-container"
+              className="bg-gray-300 w-fit h-fit overflow-hidden flex flex-row justify-between items-center transition-all duration-1000 ease-in rounded-b-md
           "
-          >
-            <button
-              id="yes-button"
-              className="hidden w-fit h-fit font-semibold text-md text-white hover:text-black p-2 m-2 rounded-md bg-red-600 hover:bg-red-400  hover:border-green-300"
-              onClick={() => deleteClick('deletePet')}
             >
-              Delete Pet
-            </button>
-            <button
-              id="no-button"
-              className="hidden w-fit h-fit font-semibold text-md text-white hover:text-black p-2 m-2 rounded-md bg-cyan-600 hover:bg-cyan-400 hover:border-green-300"
-              onClick={() => deleteClick('cancel')}
-            >
-              Cancel
-            </button>
+              <button
+                id="yes-button"
+                className="hidden w-fit h-fit font-semibold text-md text-white hover:text-black p-2 m-2 rounded-md bg-red-600 hover:bg-red-400  hover:border-green-300"
+                onClick={() => deleteClick('deletePet')}
+              >
+                Delete Pet
+              </button>
+              <button
+                id="no-button"
+                className="hidden w-fit h-fit font-semibold text-md text-white hover:text-black p-2 m-2 rounded-md bg-cyan-600 hover:bg-cyan-400 hover:border-green-300"
+                onClick={() => deleteClick('cancel')}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
 
       {/* Image / Name section */}
       <section className="w-full h-fit flex flex-col xs:flex-row justify-between items-center px-0 xxs:px-1 xs:px-2 sm:px-5">
@@ -131,22 +133,27 @@ const PetProfile = (petData: petProfileProps) => {
             className="contain"
           />
         </div>
+
+        {/* Pet Name */}
         <div className="relative w-fit">
           <h2
             className="font-bold text-center xs:text-end w-full sm:w-fit mx:auto sm:mx-0"
             style={{ fontSize: 'calc(20px + 6vmin)' }}
           >
-            {petName.text}
+            {petName.public ? petName.text : `You've found someone's pet!`}
           </h2>
-          <p
-            className={`text-xs w-fit text-end ${
-              petName!.public ? 'text-green-500' : 'text-red-500'
-            }`}
-          >
-            {petName.public
-              ? 'Visible when scanned'
-              : 'Not visible when scanned'}
-          </p>
+          {owner && (
+            // Pet Name Public?
+            <p
+              className={`text-xs w-fit text-end ${
+                petName!.public ? 'text-green-500' : 'text-red-500'
+              }`}
+            >
+              {petName.public
+                ? 'Visible when scanned'
+                : 'Not visible when scanned'}
+            </p>
+          )}
         </div>
       </section>
 
@@ -162,17 +169,19 @@ const PetProfile = (petData: petProfileProps) => {
           <p className="w-full text-lg">
             {message!.message ? message!.message : 'Not Provided'}
           </p>
-          <div className="">
-            <p
-              className={`text-xs w-fit text-start ${
-                message!.public ? 'text-green-500' : 'text-red-500'
-              }`}
-            >
-              {message!.public
-                ? 'Visible when scanned'
-                : 'Not visible when scanned'}
-            </p>
-          </div>
+          {owner && (
+            <div className="">
+              <p
+                className={`text-xs w-fit text-start ${
+                  message!.public ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {message!.public
+                  ? 'Visible when scanned'
+                  : 'Not visible when scanned'}
+              </p>
+            </div>
+          )}
         </div>
       </section>
 
@@ -191,28 +200,32 @@ const PetProfile = (petData: petProfileProps) => {
               <td className="border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2">
                 {dob?.birthday ? dob?.birthday : 'N/A'}
               </td>
-              <td
-                className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
-                  dob?.public ? 'text-green-500' : 'text-red-500'
-                }`}
-              >
-                {dob?.public ? 'Visible' : 'Not Visible'}
-              </td>
+              {owner && (
+                <td
+                  className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
+                    dob?.public ? 'text-green-500' : 'text-red-500'
+                  }`}
+                >
+                  {dob?.public ? 'Visible' : 'Not Visible'}
+                </td>
+              )}
             </tr>
 
             {/* Pet Type */}
             <tr className="border-[1px] border-gray-400">
               <td className="border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2">
-                petType
+                Type of pet
               </td>
               <td className="border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 capitalize">
                 {petType ? petType : 'N/A'}
               </td>
-              <td
-                className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 text-green-500`}
-              >
-                Visible
-              </td>
+              {owner && (
+                <td
+                  className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 text-green-500`}
+                >
+                  Visible
+                </td>
+              )}
             </tr>
 
             {/* Breed */}
@@ -223,13 +236,15 @@ const PetProfile = (petData: petProfileProps) => {
               <td className="border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2">
                 {breed?.text ? breed?.text : 'N/A'}
               </td>
-              <td
-                className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
-                  breed?.public ? 'text-green-500' : 'text-red-500'
-                }`}
-              >
-                {breed?.public ? 'Visible' : 'Not Visible'}
-              </td>
+              {owner && (
+                <td
+                  className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
+                    breed?.public ? 'text-green-500' : 'text-red-500'
+                  }`}
+                >
+                  {breed?.public ? 'Visible' : 'Not Visible'}
+                </td>
+              )}
             </tr>
 
             {/* Color */}
@@ -240,13 +255,15 @@ const PetProfile = (petData: petProfileProps) => {
               <td className="border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2">
                 {color?.text ? color?.text : 'N/A'}
               </td>
-              <td
-                className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
-                  color?.public ? 'text-green-500' : 'text-red-500'
-                }`}
-              >
-                {color?.public ? 'Visible' : 'Not Visible'}
-              </td>
+              {owner && (
+                <td
+                  className={`border-[1px] border-gray-400 w-fit h-fit text-center px-4 py-2 ${
+                    color?.public ? 'text-green-500' : 'text-red-500'
+                  }`}
+                >
+                  {color?.public ? 'Visible' : 'Not Visible'}
+                </td>
+              )}
             </tr>
           </tbody>
         </table>
@@ -273,17 +290,19 @@ const PetProfile = (petData: petProfileProps) => {
               'Not Provided'
             )}
           </p>
-          <div className="">
-            <p
-              className={`text-xs w-fit text-start ${
-                homeAddress!.public ? 'text-green-500' : 'text-red-500'
-              }`}
-            >
-              {homeAddress!.public
-                ? 'Visible when scanned'
-                : 'Not visible when scanned'}
-            </p>
-          </div>
+          {owner && (
+            <div className="">
+              <p
+                className={`text-xs w-fit text-start ${
+                  homeAddress!.public ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {homeAddress!.public
+                  ? 'Visible when scanned'
+                  : 'Not visible when scanned'}
+              </p>
+            </div>
+          )}
         </div>
         <hr className="w-full h-[2px] my-4" />
         {/* What 3 Words Address Section */}
@@ -302,17 +321,19 @@ const PetProfile = (petData: petProfileProps) => {
               'Not Provided'
             )}
           </p>
-          <div className="">
-            <p
-              className={`text-xs w-fit text-start ${
-                what3words!.public ? 'text-green-500' : 'text-red-500'
-              }`}
-            >
-              {what3words!.public
-                ? 'Visible when scanned'
-                : 'Not visible when scanned'}
-            </p>
-          </div>
+          {owner && (
+            <div className="">
+              <p
+                className={`text-xs w-fit text-start ${
+                  what3words!.public ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {what3words!.public
+                  ? 'Visible when scanned'
+                  : 'Not visible when scanned'}
+              </p>
+            </div>
+          )}
         </div>
       </section>
 
@@ -331,17 +352,19 @@ const PetProfile = (petData: petProfileProps) => {
               {contactNumber!.phone ? contactNumber!.phone : 'Not Provided'}
             </span>
           </a>
-          <div className="">
-            <p
-              className={`text-xs w-fit text-start ${
-                contactNumber!.public ? 'text-green-500' : 'text-red-500'
-              }`}
-            >
-              {contactNumber!.public
-                ? 'Visible when scanned'
-                : 'Not visible when scanned'}
-            </p>
-          </div>
+          {owner && (
+            <div className="">
+              <p
+                className={`text-xs w-fit text-start ${
+                  contactNumber!.public ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {contactNumber!.public
+                  ? 'Visible when scanned'
+                  : 'Not visible when scanned'}
+              </p>
+            </div>
+          )}
         </div>
         <hr className="w-full h-[2px] my-4" />
         {/* Email */}
@@ -350,78 +373,83 @@ const PetProfile = (petData: petProfileProps) => {
             Contact Email:&nbsp;
             {contactEmail!.email ? contactEmail!.email : 'Not Provided'}
           </a>
-          <div className="">
-            <p
-              className={`text-xs w-fit text-start ${
-                contactEmail!.public ? 'text-green-500' : 'text-red-500'
-              }`}
-            >
-              {contactEmail!.public
-                ? 'Visible when scanned'
-                : 'Not visible when scanned'}
-            </p>
-          </div>
+          {owner && (
+            <div className="">
+              <p
+                className={`text-xs w-fit text-start ${
+                  contactEmail!.public ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {contactEmail!.public
+                  ? 'Visible when scanned'
+                  : 'Not visible when scanned'}
+              </p>
+            </div>
+          )}
         </div>
       </section>
 
       <hr className="w-full h-[2px] my-4" />
-
-      {/* Scan History */}
-      <section className="w-full h-fit flex flex-col justify-between items-center py-6 px-0 xxs:px-1 xs:px-2 sm:px-5 relative">
-        <h2 className="w-full h-fit py-2 font-semibold text-2xl">
-          Scan History
-        </h2>
-        {scanHistory!.map(
-          ({
-            dateTime,
-            coordinates,
-            message,
-            scannerName,
-            contactDetail,
-          }: petScanHistory) => (
-            <ScanHistoryItem
-              key={dateTime.replace(' ', '')}
-              dateTime={dateTime}
-              coordinates={coordinates}
-              message={message}
-              scannerName={scannerName}
-              contactDetail={contactDetail}
+      {owner && (
+        <>
+          // Scan History
+          <section className="w-full h-fit flex flex-col justify-between items-center py-6 px-0 xxs:px-1 xs:px-2 sm:px-5 relative">
+            <h2 className="w-full h-fit py-2 font-semibold text-2xl">
+              Scan History
+            </h2>
+            {scanHistory!.map(
+              ({
+                dateTime,
+                coordinates,
+                message,
+                scannerName,
+                contactDetail,
+              }: petScanHistory) => (
+                <ScanHistoryItem
+                  key={dateTime.replace(' ', '')}
+                  dateTime={dateTime}
+                  coordinates={coordinates}
+                  message={message}
+                  scannerName={scannerName}
+                  contactDetail={contactDetail}
+                />
+              )
+            )}
+          </section>
+          <hr className="w-full h-[2px] my-4" />
+          // QR Code
+          <section className="w-full h-fit flex flex-col justify-between items-center py-6 px-0 xxs:px-1 xs:px-2 sm:px-5 relative">
+            <h2 className="w-full h-fit py-2 font-semibold text-2xl">
+              Personal QR code.
+            </h2>
+            <p className="w-full h-fit font-normal text-md">
+              Download you QR code and have it engraved anywhere you want!{' '}
+              <span className="text-gray-500">
+                e.g collar, harness, lead etc.
+              </span>
+            </p>
+            <p className="w-full h-fit font-normal text-md">
+              This QR Code will lead to:&nbsp;
+              <a
+                href={`${websiteUrl}/pet-found?id=${_id}`}
+                aria-label="personal pet qr code link"
+                target="_blank"
+                rel="noopener"
+                className="hover:text-primary underline text-sm"
+              >
+                {websiteUrl}/pet-found?id={_id}
+              </a>
+            </p>
+            <p className="w-full h-fit text-sm text-gray-400 mb-2">
+              *The QR Code may change, but previous ones will still work.
+            </p>
+            <QrCode
+              urlToLinkTo={`${websiteUrl}/pet-found?id=${_id}`}
+              qrCodeWidth="100%"
             />
-          )
-        )}
-      </section>
-
-      <hr className="w-full h-[2px] my-4" />
-
-      {/* QR Code */}
-      <section className="w-full h-fit flex flex-col justify-between items-center py-6 px-0 xxs:px-1 xs:px-2 sm:px-5 relative">
-        <h2 className="w-full h-fit py-2 font-semibold text-2xl">
-          Personal QR code.
-        </h2>
-        <p className="w-full h-fit font-normal text-md">
-          Download you QR code and have it engraved anywhere you want!{' '}
-          <span className="text-gray-500">e.g collar, harness, lead etc.</span>
-        </p>
-        <p className="w-full h-fit font-normal text-md">
-          This QR Code will lead to:&nbsp;
-          <a
-            href={`${websiteUrl}/pet-found?id=${_id}`}
-            aria-label="personal pet qr code link"
-            target="_blank"
-            rel="noopener"
-            className="hover:text-primary underline text-sm"
-          >
-            {websiteUrl}/pet-found?id={_id}
-          </a>
-        </p>
-        <p className="w-full h-fit text-sm text-gray-400 mb-2">
-          *The QR Code may change, but previous ones will still work.
-        </p>
-        <QrCode
-          urlToLinkTo={`${websiteUrl}/pet-found?id=${_id}`}
-          qrCodeWidth="100%"
-        />
-      </section>
+          </section>
+        </>
+      )}
     </div>
   );
 };
