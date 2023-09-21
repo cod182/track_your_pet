b421484947f6714ed85cf59a8488ea8c6db0ccf5
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-09-21 14:49:12 +0100

    added attribution page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full h-[50px] border-t-2 border-black">Footer</footer>
+    <footer className="w-full h-[50px] border-t-2 border-black">
+      <ul className="flex flex-row flex-wrap justify-start items-center">
+        <li className="mx-2">TrackYouPet - Educational Purposes Only</li>
+        <li className="mx-2">
+          <a className="underline hover:text-primary" href="/attribution">
+            Attribution
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 };
 
